15a6627b4af2d5a4b30c1c200f79d1a4920f4ba4
MN-TG <mohammedmusammiln@gmail.com>
2025-05-20 19:40:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 CHANNELS = list(map(int, os.environ.get("CHANNELS", "-1001234567890").split()))
+
+REDIRECT_CHANNEL = -100xxxxxxxxxx
+REDIRECT_INVITE = "https://t.me/+XXXXXX"  # Your public/private invite link
